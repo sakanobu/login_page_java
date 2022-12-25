50a71f00159b187dfe133e0e4bd1f3153e2baf2f
sakanobu <grapejuice100percent@gmail.com>
2022-12-25 23:06:21 +0900

    Userクラスの追加

---
+++ src/main/java/login/User.java
@@ -0,0 +1,25 @@
+package login;
+
+public class User {
+  private int id;
+  private String name;
+  private String password;
+
+  User(int id, String name, String password) {
+    this.id = id;
+    this.name = name;
+    this.password = password;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+}
