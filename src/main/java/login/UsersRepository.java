06326e602db5d2277a9e6a6f40c718d139079816
sakanobu <grapejuice100percent@gmail.com>
2023-01-03 15:16:56 +0900

    findAll()メソッドの実装

--- src/main/java/login/UsersRepository.java
+++ src/main/java/login/UsersRepository.java
@@ -6,23 +6,74 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class UsersRepository {
   private static final String URL = "jdbc:mysql://localhost:3306/login_page_study";
   private static final String DB_USER_NAME = "login_page_study_user";
   private static final String DB_PASSWORD = Dotenv.load().get("MYSQL_PASSWORD");
+  private Connection con = null;
+  private PreparedStatement ps = null;
+  private ResultSet rs = null;
+
 
   public static void main(String[] args) {
     UsersRepository usersRepository = new UsersRepository();
+
+    ArrayList<User> allUserList = usersRepository.findAll();
+
+    System.out.println("↓ findAll()");
+    for (User user : allUserList
+    ) {
+      System.out.printf("%d, %s, %s\n", user.getId(), user.getName(), user.getPassword());
+    }
+
     User user = usersRepository.findUserById("1");
+    System.out.println("\n↓ findUserById()");
     System.out.println("%d, %s, %s".formatted(user.getId(), user.getName(), user.getPassword()));
   }
 
-  public User findUserById(String targetId) {
-    Connection con = null;
-    PreparedStatement ps = null;
-    ResultSet rs = null;
+  public ArrayList<User> findAll() {
+    ArrayList<User> allUserList = new ArrayList<>();
 
+    String sql = """
+        SELECT u.id, u.name, u.password FROM users as u
+        """;
+
+    try {
+      con = DriverManager.getConnection(URL, DB_USER_NAME, DB_PASSWORD);
+      ps = con.prepareStatement(sql);
+      rs = ps.executeQuery();
+
+      while (rs.next()) {
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        String password = rs.getString("password");
+
+        allUserList.add(new User(id, name, password));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (rs != null) {
+          rs.close();
+        }
+        if (ps != null) {
+          ps.close();
+        }
+        if (con != null) {
+          con.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return allUserList;
+  }
+
+  public User findUserById(String targetId) {
     int id = -1;
     String name = "";
     String password = "";
